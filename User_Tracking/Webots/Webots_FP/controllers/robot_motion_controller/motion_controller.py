d06bfd77685e31d2170650c61790d2121898fd3f
jtgrotz <jordantaylorg007@gmail.com>
2024-03-31 21:28:46 -0400

    making progress on the trajectory planning

--- User_Tracking/Webots/Webots_FP/controllers/robot_motion_controller/motion_controller.py
+++ User_Tracking/Webots/Webots_FP/controllers/robot_motion_controller/motion_controller.py
@@ -8,7 +8,10 @@ class MotionController:
         self.outside_wheel_front = wheel2 #front right
         self.outside_wheel_back = wheel3 #back left
 
-        self.current_position = [0,0]
+        self.current_position = [0,0,0]
+        self.current_velocity = [0,0,0]
+        self.goal_position = [0,0,0]
+        self.error_epsilon = 0.1
         self.current_heading = 0
         self.wheel_radius = 3.15
         self.l_x = 0.5
@@ -88,7 +91,13 @@ class MotionController:
     def get_encoder_reading(self, wheel):
         return 0
 
-    def at_goal_position(self, goal_position):
+    def at_goal_position(self):
+        if abs(self.current_position[0]-self.goal_position[0]) < self.error_epsilon:
+            if abs(self.current_position[1]-self.goal_position[1]) < self.error_epsilon:
+                if abs(self.current_position[2]-self.goal_position[2]) < self.error_epsilon*5: #heading error is less important
+                    return True
+
+        return False
         #checkfoward kinematics more weight
         #check GPS position less weight
 
@@ -108,3 +117,29 @@ class MotionController:
         vels = np.array([x_vel, y_vel, z_vel]).reshape(3,1)
         [wfl, wfr, wbl, wbr] = np.matmul(inv_kin, vels)
         return [wfl, wfr, wbl, wbr]
+
+
+    def quintic_trajectory(self, x_i, x_f, xd_i, xd_f, xdd_i, xdd_f, t, steps):
+        inputs = np.array([x_i, x_f, xd_i, xd_f, xdd_i, xdd_f]).reshape(6,1)
+
+        matrix = np.array([[0,0,0,0,0,1],
+                           [np.power(t,5), np.power(t,4), np.power(t,3), np.power(t,2), 1],
+                          [0,0,0,0,1,0],
+                          [5*np.power(t,4), 4*np.power(t,3), 3*np.power(t,2), 2*t, 1, 0],
+                          [0,0,0,2,0,0],
+                          [20*np.power(t,3), 12*np.power(t,2), 6*t, 2, 0, 0]])
+        coeffs = np.matmul(np.linalg.inv(matrix), inputs)
+
+        #linear spacing of trajectory
+        time_steps = np.linspace(0, t, steps)
+        x_traj = coeffs[0]*np.power(time_steps,5)+coeffs[1]*np.power(time_steps,4)+coeffs[2]*np.power(time_steps,3)+coeffs[3]*np.power(time_steps,2)+coeffs[4]*time_steps+coeffs[5]
+        xd_traj = 5*coeffs[0]*np.power(time_steps,4)+4*coeffs[1]*np.power(time_steps,3)+3*coeffs[2]*np.power(time_steps,2)+2*coeffs[3]*time_steps+coeffs[4]
+        xdd_traj = 20*coeffs[0]*np.power(time_steps,3)+12*coeffs[1]*np.power(time_steps,2)+6*coeffs[2]*time_steps+2*coeffs[3]
+
+        return x_traj, xd_traj, xdd_traj
+
+    def plan_trajectory(self, final_points, end_velocity, time):
+        [x_traj, xd_traj, xdd_traj] = self.quintic_trajectory(self.current_position[0], final_points[0], self.current_velocity[0],end_velocity[0],0,0,time, 10)
+        [y_traj, yd_traj, ydd_traj] = self.quintic_trajectory(self.current_position[1], final_points[1], self.current_velocity[1],end_velocity[1],0,0,time, 10)
+        [w_traj, wd_traj, wdd_traj] = self.quintic_trajectory(self.current_position[2], final_points[2], self.current_velocity[2],end_velocity[2],0,0,time, 10)
+

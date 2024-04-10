95da0383f75600b9bf0275f36918e61bcf64c620
jtgrotz <jordantaylorg007@gmail.com>
2024-04-09 21:06:08 -0400

    created world with a bunch of objects to test developed code for obstacle deteciton

--- obstacle_detection/Python/obstacle_detector.py
+++ obstacle_detection/Python/obstacle_detector.py
@@ -4,6 +4,8 @@ import numpy as np
 class ObstacleDetector:
     def __init__(self, camera, camera_transform, sample_period, lidar=None, lidar_transform=None):
         self.cam = camera #camera transform from webots
+        self.camera_width = camera.getWidth()
+        self.camera_height = camera.getHeight()
         self.lidar = lidar #lidar object from webots
         self.cam_transform = camera_transform # transformation from robot center to camera frame
         self.lidar_transform = lidar_transform # transformation from robot center to lidar frame
@@ -15,7 +17,7 @@ class ObstacleDetector:
 
 
         #dictionary for storing different camera objects seen
-        self.camera_objects = []
+        self.camera_objects = {}
 
         #set lidar settings
         if self.lidar is not None:
@@ -29,7 +31,31 @@ class ObstacleDetector:
     #returns a list of objects with their ID, world location, estimated_radius, estimated_velocity, description, relative location to the robot
     def get_camera_obstacles(self):
         #should pull from an internally set list that is updated occasionally, instead of pulling only when its updated?
-        return 0
+        detected_obstacles = self.cam.getRecognitionObjects()
+        for objects in detected_obstacles:
+            id = objects.getId()
+            #check if we have seen the object before and update position/velocity
+            if self.camera_objects.get(id) is not None:
+                data = self.camera_objects[id]
+                prev_position = data[1] #TODO
+                new_position = self.transform_camera_object_position(objects.getPosition())
+                self.calc_velocity(prev_position, new_position)
+                #make new data for dict and update #TODO
+                self.camera_objects.update({id: data})
+            else:
+                id = objects.getId()
+                position = objects.getPosition()
+                transformed_position = self.transform_camera_objects(position)
+                size = objects.getSize()  # adjust based on size
+                velocity = [0,0,0]
+                description = objects.getModel()
+                relative_location = self.get_relative_location(objects)
+                new_data = [id, position, size, velocity, description, relative_location]
+                self.camera_objects[id] = new_data
+
+        #prune obstacle list to get rid of old obstacles
+        self.prune_obstacles(10, self.robot_position)
+        return self.camera_objects.copy()
 
     #translates lidar point cloud to obstacles in the configuration space.
     def get_lidar_obstacles(self):
@@ -40,11 +66,10 @@ class ObstacleDetector:
             return 0
 
     #helper functions
-    def transform_camera_objects(self, camera_objects):
-        #get camera objects
-        #extract position and update dictionary
-        #prune dictionary for obstacles that are far outside of the range of the robot
-        return 0
+    def transform_camera_object_position(self, position):
+        # transform position
+        new_position = np.matmul(self.cam_transform, np.array([position[0], position[1],position[3],1])).reshape(4,1)
+        return [new_position[0], new_position[1], new_position[2]]
 
     #need to decide if we are using the point cloud or range image of the class. The range image is faster, and closer to what we would get with a real?
     #but
@@ -58,8 +83,24 @@ class ObstacleDetector:
         #iterate through the list of obstacles, and remove if far away from the robot.
         return 0
 
-    def calculate_obstacle_velocity(self):
-        return 0
+    def calculate_obstacle_velocity(self, prev_position, new_position):
+        xv = (new_position[0] - prev_position[0])/self.timestep
+        yv = (new_position[1] - prev_position[1])/self.timestep
+        zv = (new_position[2] - prev_position[2])/self.timestep
+        return [xv,yv,zv]
+
+    def get_relative_location(self, objects, camera_location):
+        #for multiple cameras, bin left and right side obstacles to always return their relative location
+        if camera_location == "Left Side":
+            return "Left"
+        elif camera_location == "Right Side":
+            return "Right"
+        else: #find what side of the image they are on
+            camera_location = objects.getPositionOnImage()
+            if camera_location[0] <= self.camera_width/2:
+                return "Left"
+            else:
+                return "Right"
 
 
 
